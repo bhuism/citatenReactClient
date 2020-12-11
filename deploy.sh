b6d18a5a97aef90610c3149d50934e0c39b73580
bhuism <bhuism@odee.net>
2020-12-11 17:20:18 +0100

    dont remove and create bucket

--- deploy.sh
+++ deploy.sh
@@ -4,8 +4,11 @@ set -o xtrace
 
 BUCKET=gs://citaten.odee.net
 
-gsutil -m rm -r $BUCKET
-gsutil mb -p badge-260212 -l europe-west4 -b on $BUCKET
+
+gsutil rm $BUCKET/**
+#gsutil -m rm -r $BUCKET
+#gsutil mb -p badge-260212 -l europe-west4 -b on $BUCKET
+#gsutil iam ch allUsers:objectViewer $BUCKET
 gsutil -m -h Cache-Control:public,max-age=3600 cp -r -Z build/. $BUCKET
 gsutil setmeta -h "Cache-Control:no-cache, max-age=0" -h "Content-Type:application/json" $BUCKET/actuator/info
-gsutil iam ch allUsers:objectViewer $BUCKET
+
