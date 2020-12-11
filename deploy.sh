5a6c890f6f252428f05e5a5eb692ff12cfaac113
bhuism <bhuism@odee.net>
2020-12-11 22:27:14 +0100

    cleanups

--- deploy.sh
+++ deploy.sh
@@ -9,6 +9,6 @@ gsutil rm $BUCKET/**
 #gsutil -m rm -r $BUCKET
 #gsutil mb -p badge-260212 -l europe-west4 -b on $BUCKET
 #gsutil iam ch allUsers:objectViewer $BUCKET
-gsutil -m -h Cache-Control:public,max-age=3600 cp -r -Z build/. $BUCKET
+gsutil -m -h "Cache-Control:public,max-age=3600" cp -r -Z build/. $BUCKET
 gsutil setmeta -h "Cache-Control:no-cache, max-age=0" -h "Content-Type:application/json" $BUCKET/actuator/info
 
