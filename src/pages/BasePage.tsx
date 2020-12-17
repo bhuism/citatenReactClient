92e41399442ffdd149ed54ab501a6efb3c0a37a7
bhuism <bhuism@odee.net>
2020-12-17 23:22:48 +0100

    add api badge

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -8,6 +8,9 @@ const BasePage: React.FC = ({children}) => {
         <>
             <main>{children}</main>
             <footer className='layout-footer'>
+                <a href="https://github.com/bhuism/citaten" target="_blank" rel="noreferrer">
+                    <Badge url={'https://badge.odee.net/github/actuator/bhuism/citaten/master/badge.svg?actuator_url=https://api-citaten.odee.net/actuator/info'} />
+                </a>
                 <a href="https://github.com/bhuism/citatenReactClient" target="_blank" rel="noreferrer">
                     <Badge url={'https://badge.odee.net/github/sha/bhuism/citatenReactClient/master/' + process.env.REACT_APP_GIT_SHA + '/badge.svg'}/>
                 </a>
