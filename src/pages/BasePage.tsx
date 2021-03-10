9380214eb1faa58524402fdc81ae4a2dfe828bee
bhuism <bhuism@odee.net>
2021-03-10 14:40:32 +0100

    move to html_url

--- src/pages/BasePage.tsx
+++ src/pages/BasePage.tsx
@@ -14,7 +14,7 @@ const BasePage: React.FC = ({children}) => {
                 </a>
                 <a href="https://github.com/bhuism/citatenReactClient" target="_blank" rel="noreferrer">
                     <Badge
-                        url={'https://badge.odee.net/github/actuator/bhuism/citatenReactClient/master/badge.svg?actuator_url=https://citaten.odee.net/actuator/info'}/>
+                        url={'https://badge.odee.net/github/actuator/bhuism/citatenReactClient/master/badge.svg?html_url=https://citaten.odee.net'}/>
                 </a>
             </footer>
         </>
